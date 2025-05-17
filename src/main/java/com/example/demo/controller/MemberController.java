34b61b5ed443daf18e06f230a8b0f67394fee888
manh <manh99294@donga.edu.vn>
2025-05-17 11:18:03 +0700

    in ra chi tiet thanh vien

--- src/main/java/com/example/demo/controller/MemberController.java
+++ src/main/java/com/example/demo/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.example.demo.model.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.Arrays;
 import java.util.List;
@@ -11,14 +12,21 @@ import java.util.List;
 @Controller
 public class MemberController {
 
+    List<Member> members = Arrays.asList(
+            new Member("Nguyễn Đức Mạnh",1,21,"manh99294@donga.edu.vn"),
+            new Member("Nguyễn Đình Cang",2,21,"Cang@donga.edu.vn"),
+            new Member("Huỳnh Hữu Nghĩa",3,21,"Nghia@donga.edu.vn")
+    );
     @GetMapping("/members")
     public String showMembers(Model model) {
-        List<Member> members = Arrays.asList(
-                new Member("Nguyễn Đức Mạnh"),
-                new Member("Nguyễn Đình Cang "),
-                new Member("Huỳnh Hữu Nghĩa")
-        );
         model.addAttribute("members", members);
-        return "printMember"; // Trả về file templates/members.html
+        return "printMember";
+    }
+
+    @GetMapping("/members/{id}")
+    public String showMember(@PathVariable int id, Model model) {
+        Member found = members.stream().filter(m -> m.getId() == id).findFirst().orElse(null);
+        model.addAttribute("member", found);
+        return "member";
     }
 }
--- src/main/java/com/example/demo/model/Member.java
+++ src/main/java/com/example/demo/model/Member.java
@@ -2,12 +2,44 @@ package com.example.demo.model;
 
 public class Member {
     private String name;
+    private int id;
+    private int age;
+    private String email;
 
-    public Member(String name) {
+    public Member(String name, int id, int age, String email) {
         this.name = name;
+        this.id = id;
+        this.age = age;
+        this.email = email;
     }
 
     public String getName() {
         return name;
+    }
+
+    public void setName(String name) {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
